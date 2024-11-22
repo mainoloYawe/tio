6c8c33497173d0d0b3f2bf23b0baf6fe2ca516dc
mainoloYawe <220712047@students.uajy.ac.id>
2024-11-22 23:23:19 +0700

    Update Tugas_Modul5_A_SATY_12045.py

--- Tugas_Modul5_A_SATY_12045.py
+++ Tugas_Modul5_A_SATY_12045.py
@@ -4,8 +4,8 @@ import pickle
 from PIL import Image
 import os
 
-model_directory = r'D:\tio'
-model_path = os.path.join(model_directory, r'best_model.pkl')
+
+model_path = r'best_model.pkl'
 
 if os.path.exists(model_path):
     try:
